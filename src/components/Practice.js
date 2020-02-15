56861ef149c4b870b808f174efcd2017ef85e73f
khurs10101 <khurs10101@gmail.com>
2020-02-15 06:58:22 +0530

    Adding Test Module component

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import QuestionContainer from './test_module/QuestionContainer'
 
 const Practice= ()=>(
-    <div>Welcome to Practice</div>
+    <div>
+        <Row>
+            <Col lg={4}>
+            </Col>
+            <Col lg={8}>
+                <QuestionContainer/>
+            </Col>
+        </Row>
+    </div>
 )
 
 export default Practice
--- src/components/test_module/QuestionContainer.js
+++ src/components/test_module/QuestionContainer.js
@@ -1,1 +1,13 @@
 import React from 'react'
+import SingleQuestion from './SingleQuestion'
+
+const sample= ['A', 'B', 'C', 'D', 'E', 'F']
+const QuestionContainer= ()=>(
+    <div>
+        {sample.map((data, index)=>(
+            <SingleQuestion key={index} />
+        ))}
+    </div>
+)
+
+export default QuestionContainer
